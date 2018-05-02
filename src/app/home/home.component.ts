3a8dfc5a50a16a890bf43364606b42d9db985f49
DopeHat82 <DopeHat82@gmail.com>
2018-05-02 17:10:38 -0500

    another commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { NgbTabset } from "@ng-bootstrap/ng-bootstrap";
+import{ ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('t') t;
+  
+  section: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private cdRef : ChangeDetectorRef) { 
+    this.route.queryParams.subscribe(params => {
+        this.section = params['section'];
+    });
+  }
 
   ngOnInit() {
+    
+  }
+  
+  detectIncomingTabRef = function(){
+    if(this.section){
+      this.t.select(this.section);
+      this.cdRef.detectChanges(); 
+    }
+  }
+
+  ngAfterViewInit()
+  {
+    this.detectIncomingTabRef();
+
   }
 
 }
