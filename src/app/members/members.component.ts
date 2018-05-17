102f86ebe29f52a3c84bed7a7f9f113113f0efe3
DopeHat82 <DopeHat82@gmail.com>
2018-05-17 08:35:14 -0500

    commt 11

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -15,7 +15,8 @@ userInfo=[];
 userData= {};
 applicantObj:object = {};
 success: '0';
-
+newBlogSuccess: "";
+newSexActSuccess : "";
   constructor(private http: Http) { }
 
   isAuthenticated = function(){
@@ -64,8 +65,118 @@ success: '0';
     }) 
   }
 
+  releveantAds=[];
+  fetchReleveantAds = function(){
+     this.http.get("https://s3.us-east-2.amazonaws.com/v4k-content-provider/ads.json").subscribe(
+       (res: Response) => {
+         var filter = _.filter(res.json(), function(ads)
+         {return ads.genre === "General" && ads.type === "coupons"})
+         this.releveantAds= filter; 
+         this.mediaSwitch = "true"; 
+       }
+     );
+   }
+
+   recentActivity=[];
+   fetchRecentActivity = function(){
+      this.http.get("http://18.221.222.34/recentactivity").subscribe(
+        (res: Response) => {
+          this.recentActivity= res.json(); 
+          this.mediaSwitch = "true"; 
+        }
+      );
+    }
+
+    addActivityObj:object = {};
+    addActivity = function(appData){
+      
+      this.addActivityObj = {
+      "description" : appData.newActivity 
+      }
+      this.http.post("http://18.221.222.34/recentactivity", this.addActivityObj, {headers: {
+        'Content-Type': 'application/json' , 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers':'X-Requested-With'	
+    }}).subscribe((res:Response) => {
+        this.isAdded = true; 
+  
+      }) 
+    }
+
+
+
+    theNews=[];
+    fetchNews = function(){
+       this.http.get("http://18.221.222.34/news").subscribe(
+         (res: Response) => {
+           this.theNews= res.json(); 
+           this.mediaSwitch = "true"; 
+         }
+       );
+     }
+ 
+     addNewsObj:object = {};
+     addNews = function(appData){
+       
+       this.addNewsObj = {
+       "title" : appData.newTitle,
+       "description" : appData.newArticle,
+       "timestamp" : (new Date).toLocaleDateString().toString()
+       }
+       this.http.post("http://18.221.222.34/news", this.addNewsObj, {headers: {
+         'Content-Type': 'application/json' , 
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+         'Access-Control-Allow-Headers':'X-Requested-With'	
+     }}).subscribe((res:Response) => {
+         this.isAdded = true; 
+   
+       }) 
+     }
+
+     addNewBlogObj:object = {};
+     addNewBlog = function(appData){
+       
+       this.addNewBlogObj = {
+       "submittedBy" : this.userData.userName,
+       "title" : appData.newBlogTitle,
+       "description" : appData.newBlogArticle,
+       "timestamp" : (new Date).toLocaleDateString().toString()
+       }
+       this.http.post("http://18.221.222.34/submissions", this.addNewBlogObj, {headers: {
+         'Content-Type': 'application/json' , 
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+         'Access-Control-Allow-Headers':'X-Requested-With'	
+     }}).subscribe((res:Response) => {
+         this.isAdded = true; this.newBlogSuccess = 'Thank you! Your submission will be reviewed and we will notify you of its acceptance status.';
+   
+       }) 
+     }
+
+     addNewSexActObj:object = {};
+     addNewSexAct = function(appData){
+       
+       this.addNewBlogObj = {
+       "submittedBy" : this.userData.userName,
+       "title" : appData.newSexActTitle,
+       "description" : appData.newSexActArticle,
+       "timestamp" : (new Date).toLocaleDateString().toString()
+       }
+       this.http.post("http://18.221.222.34/submissions", this.addNewBlogObj, {headers: {
+         'Content-Type': 'application/json' , 
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+         'Access-Control-Allow-Headers':'X-Requested-With'	
+     }}).subscribe((res:Response) => {
+         this.isAdded = true; this.newSexActSuccess = 'Thank you! Your submission will be reviewed and we will notify you of its acceptance status.';
+   
+       }) 
+     }
+
   ngOnInit() {
-    this.isAuthenticated(); 
+    this.isAuthenticated(); this.fetchReleveantAds(); this.fetchRecentActivity(); this.fetchNews();
   }
 
 }
